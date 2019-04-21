15a566bfe454e19913127f3e6fc159cf900be273
Bruno Bacelar <39466847+TheDigitalPumpkin@users.noreply.github.com>
2019-04-21 15:29:52 -0300

    Create coin_collector.cpp

---
+++ coin_collector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t, n, dados[1001];
+    scanf("%d", &t);
+    
+    while(t--) 
+	{
+        scanf("%d", &n);
+        
+        for (int i = 0; i < n; i++) 
+			scanf("%d", dados + i);
+        
+        int resultado = 0, soma = 0;
+        
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if(soma + dados[i] < dados[i + 1])
+            ++resultado, soma += dados[i];
+        }
+        
+        ++resultado;
+        printf("%d\n", resultado);
+    }
+    
+    return 0;
+}
